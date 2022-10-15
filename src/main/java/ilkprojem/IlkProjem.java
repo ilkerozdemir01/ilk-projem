4fbf7c379bbef184f85fff7dbb35728bad03f3ed
ilkerozdemir01 <115585933+ilkerozdemir01@users.noreply.github.com>
2022-10-15 14:50:29 +0300

    ilkdegisikligim

--- src/main/java/ilkprojem/IlkProjem.java
+++ src/main/java/ilkprojem/IlkProjem.java
@@ -3,5 +3,7 @@ package ilkprojem;
 public class IlkProjem {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("asddas");
+        System.out.println("adsdasdaws");
     }
 }
